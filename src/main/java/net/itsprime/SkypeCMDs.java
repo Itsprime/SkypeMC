77f796d4aa8ad6c69ae5f47c5cb81801dfd9f756
BuildTools <itsprimesuarez@gmail.com>
2015-12-29 04:20:22 -0800

    Removed Skype CMDs and fixed Java JDK

--- src/main/java/net/itsprime/SkypeCMDs.java
+++ src/main/java/net/itsprime/SkypeCMDs.java
@@ -1,12 +1,8 @@
 package net.itsprime;
 
-import in.kyle.ezskypeezlife.api.obj.SkypeConversation;
 import in.kyle.ezskypeezlife.api.obj.SkypeMessage;
 import in.kyle.ezskypeezlife.events.conversation.SkypeMessageReceivedEvent;
 import org.apache.commons.lang.StringUtils;
-import org.jsoup.Jsoup;
-
-import java.net.URL;
 
 /**
  * Created by Jham on 12/29/2015.
@@ -16,6 +12,7 @@ public class SkypeCMDs {
         String message = event.getMessage().getMessage(); // Get the message content
         System.out.println("Got message: " + event.getMessage().getSender().getUsername() + " - " + message);
         String[] args = message.split(" ");
+
     switch (args[0].toLowerCase()) {
         case "+ping":  // Replies to a conversation with the message pong
             event.reply("Pong!");
@@ -37,20 +34,6 @@ public class SkypeCMDs {
                 event.reply("Usage: +topic topic");
             }
             break;
-        case "+join": // Join a conversation by the Skype join URL, eg: https://join.skype.com/rbjoWQCc9b8l
-            if (args.length > 0) {
-                String url = Jsoup.parse(StringUtils.join(args, ' ', 1, args.length)).text();
-                try {
-                    SkypeConversation conversation = ezSkype.joinSkypeConversation(url);
-                    event.reply("Joined: " + conversation.getTopic());
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                    event.reply("Error: " + event.getMessage());
-                }
-            } else {
-                event.reply("Usage: +join url");
-            }
-            break;
         case "+contact": // Set user as a contact
             if (args.length > 0) {
                 boolean contact = Boolean.parseBoolean(args[1]);
@@ -66,23 +49,6 @@ public class SkypeCMDs {
             // Calling fullyLoad will load in the important data we need if it is not already loaded
             event.getMessage().getConversation().fullyLoad();
             event.reply(event.getMessage().getConversation().toString());
-            break;
-        case "+convos": // List all the conversations the bot is in
-            event.reply(ezSkype.getConversations().toString());
-            break;
-        case "+pic": // Send a ping of an image from a URL
-            if (args.length != 0) {
-                try {
-                    String url = Jsoup.parse(StringUtils.join(args, ' ', 1, args.length)).text();
-                    event.getMessage().getConversation().sendImage(new URL(url).openStream());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    event.reply("Could not send image, check console");
-                }
-                event.reply("Sent");
-            } else {
-                event.reply("Usage: +pic url");
-            }
             break;
         }
     }
